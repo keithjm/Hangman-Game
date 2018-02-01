22c2ba81214fc594fc6ad40f0cbcdbcf12db0591
James Keith <jamiek531@gmail.com>
2018-01-31 22:14:51 -0500

    Added javascript for game

---
+++ assets/javascript/game.js
@@ -0,0 +1,74 @@
+
+//Limit for guesses in a single round
+var guessLimit = 12;
+
+//Array containing the possible answers
+var wordList = ["John Mayer", "Eric Clapton", "Radio Head"];
+var guessesRemaining;
+
+//Array that keeps all user guesses
+var userGuesses = [];
+
+//Array that contains the correct user guesses from round 
+var userCorrectGuesses = [];
+
+//word Randomly Selected to be the answer
+var solution = [];
+
+//Resets 
+function newRound() {
+    roundReset();
+    solution = wordList[Math.floor(Math.random() * wordList.length)];
+    userCorrectGuesses.length=solution.length;
+
+}   
+
+//Logic when a letter is guessed by player
+function guessLetter(guess) {
+    guess = guess.toLowerCase();
+    console.log("Solution: " + solution);
+    console.log("User guess: " + guess);
+    
+
+    //Checking if letter has been typed before
+    if(userCorrectGuesses.indexOf(guess) == -1) {
+    //User guesses correctly
+        if(solution.indexOf(guess) != -1) {
+            guessesRemaining --;
+            userGuesses.push(guess);
+            userCorrectGuesses[solution.indexOf(guess)] = guess;
+            
+        //User guesses incorrectly
+        } else if (wordList.indexOf(guess) == -1) {
+            guessesRemaining --;
+            userGuesses.push(guess);
+            console.log(userCorrectGuesses);
+        }
+    }
+}
+
+
+//Resets the game for a new round
+function roundReset() {
+    guessesRemaining = guessLimit;
+    userGuesses = [];
+
+}
+
+//When key is pressed, function runs
+document.onkeyup = function(event) {
+    if(letterCheck(event.key)) {
+        guessLetter(event.key);
+    }
+};
+
+//Check if input is letter (Not Working)
+function letterCheck(input) {
+    if((input.charCodeAt(0) >= 65 && input.charCodeAt(0) <= 90) || (input.charCodeAt(0) >= 97 && input.charCodeAt(0) <= 122)) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+
